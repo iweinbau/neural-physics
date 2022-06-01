83d358961bd9092335ef879dbd021fe0e8bf8e0e
Iwein Bau <iwein.bau@gmail.com>
2022-06-01 22:54:40 +0200

    Add PCA utility class

---
+++ math/pca.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+
+class PCA:
+
+    def __init__(self, n):
+        """
+        Create new PCA object
+        @param n: number of components
+        """
+        self.n_components = n
+        self.T = np.array([])
+        self.mean = np.array([])
+
+    def fit(self, x, mean_zero=True):
+        """
+        Calculate PCA from data x
+        @param mean_zero: true if you want to do pca with a zero mean.
+        @param x: m x n data array, where n is the number of data points and m DOF
+        """
+        self.mean = np.mean(x, axis=1, keepdims=True)
+        if mean_zero:
+            data = x - self.mean
+        else:
+            data = x
+
+        # calculate eigen values and vectors of the covariance matrix
+        cov = np.cov(data, rowvar=True)
+        eigen_val, eigen_vec = np.linalg.eig(cov)
+
+        # sort descending
+        sorted_index = np.argsort(eigen_val)[::-1]
+        sorted_eigen_vec = eigen_vec[:, sorted_index]
+        self.T = sorted_eigen_vec[:, 0:self.n_components]
+
+    def encode(self, x, mean_zero=True):
+        """
+        Compose data in to its PCA components
+        @param x: m x n data vector, where n are the number of data points and m the number of features
+        @param mean_zero: if the data should be standardised
+        @return: return an n_components x n data vector
+        """
+        if mean_zero:
+            return np.matmul((x - self.mean).T, self.T)
+        else:
+            return np.matmul(x.T, self.T)
+
+    def decode(self, x):
+        """
+        Decompose a PCA feature vector in to its full feature vector
+        @param x: n_components x n array
+        @return: m x n array with n the number of data points and m the number of features
+        """
+        return np.dot(x, self.T.T).T
+
