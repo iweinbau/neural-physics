965177358a52b546bd7ef54f584187d2fb1611a5
Iwein Bau <iwein.bau@gmail.com>
2022-06-12 19:18:45 +0200

    Added pytorch model.

--- train/subspace_neural_physics.py
+++ train/subspace_neural_physics.py
@@ -1,7 +1,38 @@
 import numpy as np
+import torch
+from torch import nn
 
 # 60 fps
 dt = 1 / 60
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+
+class SubSpaceNeuralNetwork(nn.Module):
+    def __int__(self, n_hidden_layers, n_components):
+        def input_layer(n_input, n_output):
+            return nn.Linear(n_input, n_output)
+
+        def hidden_layer(n_hidden):
+            return nn.Sequential(
+                nn.Linear(n_hidden, n_hidden),
+                nn.ReLU())
+
+        def output_layer(n_input, n_output):
+            return nn.Linear(n_input, n_output)
+
+        self.n_components = n_components
+        self.hidden_units = round(1.5 * n_components)
+        layers = [input_layer(self.n_components, self.hidden_units)]
+        for i in range(n_hidden_layers):
+            layers.append(hidden_layer(self.hidden_units))
+        layers.append(output_layer(self.hidden_units, self.n_components))
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
 
 
 def loss_position(z_star, z):
@@ -28,7 +59,7 @@ def loss_velocity(z_star, z_star_prev, z, z_prev):
 
 def loss_fn(z_star, z_star_prev, z, z_prev):
     """
-    Total loss fucntion
+    Total loss function
     @param z_star_prev: previous (s x n_components) predictions
     @param z_prev: previous (s x n_components) feature vector
     @param z_star: predicted (s x n_components) feature vector
