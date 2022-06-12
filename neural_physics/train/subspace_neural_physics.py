700c32606a7d20186d19b352ed3fdbfa989b0927
Joppe Geluykens <joppe@predibase.com>
2022-06-12 21:14:43 +0000

    create package + refactor torch model

--- neural_physics/train/subspace_neural_physics.py
+++ neural_physics/train/subspace_neural_physics.py
@@ -10,29 +10,33 @@ print(f"Using {device} device")
 
 
 class SubSpaceNeuralNetwork(nn.Module):
-    def __int__(self, n_hidden_layers, n_components):
-        def input_layer(n_input, n_output):
-            return nn.Linear(n_input, n_output)
+    def __init__(self, n_hidden_layers: int, n_components: int):
+        super().__init__()
 
-        def hidden_layer(n_hidden):
-            return nn.Sequential(
-                nn.Linear(n_hidden, n_hidden),
-                nn.ReLU())
+        hidden_units = round(1.5 * n_components)
 
-        def output_layer(n_input, n_output):
-            return nn.Linear(n_input, n_output)
+        self.encode = nn.Linear(n_components, hidden_units)
 
-        self.n_components = n_components
-        self.hidden_units = round(1.5 * n_components)
-        layers = [input_layer(self.n_components, self.hidden_units)]
-        for i in range(n_hidden_layers):
-            layers.append(hidden_layer(self.hidden_units))
-        layers.append(output_layer(self.hidden_units, self.n_components))
+        self.feed_forward = nn.Sequential(
+            *[
+                nn.Sequential(nn.Linear(hidden_units, hidden_units), nn.ReLU())
+                for _ in range(n_hidden_layers)
+            ],
+        )
 
-        self.model = nn.Sequential(*layers)
+        self.decode = nn.Linear(hidden_units, n_components)
 
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """
+        Encodes the input and then decodes it.
+
+        @param inputs: The input to the network. Shape: (batch_size, n_components)
+        @return: The output of the network. Shape: (batch_size, n_components)
+        """
+        hidden = self.encode(inputs)  # (batch_size, hidden_units)
+        hidden = self.feed_forward(hidden)  # (batch_size, hidden_units)
+        logits = self.decode(hidden)  # (batch_size, n_components)
+        return logits
 
 
 def loss_position(z_star, z):
@@ -54,7 +58,7 @@ def loss_velocity(z_star, z_star_prev, z, z_prev):
     @param z: actual (s x n_components) feature vector
     @return: 1 x n_component loss vector
     """
-    return np.mean(np.abs((z_star - z_star_prev)/dt - (z - z_prev)/dt), axis=0)
+    return np.mean(np.abs((z_star - z_star_prev) / dt - (z - z_prev) / dt), axis=0)
 
 
 def loss_fn(z_star, z_star_prev, z, z_prev):
--- train/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import subspace_neural_physics
