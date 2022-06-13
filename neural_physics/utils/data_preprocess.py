1806d8f8dbdb5599851819bc31cb65e1d5c1a2b4
Joppe Geluykens <joppe@predibase.com>
2022-06-13 12:49:41 +0000

    refactor data preprocessing

--- neural_physics/utils/data_preprocess.py
+++ neural_physics/utils/data_preprocess.py
@@ -1,39 +1,46 @@
+from typing import Tuple
+
 import numpy as np
-from core_math.alg import least_squares
+from neural_physics.core_math.alg import least_squares
 
 
-def initial_model_param(z):
+def initial_model_params(subspace_z: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """
     Calculate Alpha and Beta for the initial model by solving a least square problem for each component
     @param z: encode data vector (n_components x n)
     @return X: [alpha_m, beta_m] with m in [1, n_components]
     """
 
-    n_components, n = z.shape
-    X = np.zeros((n_components, 2))
+    num_components, num_frames = subspace_z.shape
+    X = np.zeros((num_components, 2))
 
-    for i in range(0, n_components):
-        A = np.zeros((n - 2, 2))
-        A[:, 0] = z[i, 1:n-1]
-        A[:, 1] = np.diff(z[i, 0:n-1])
-        b = z[i, 2:n]
+    for m in range(0, num_components):
+        A = np.zeros((num_frames - 2, 2))
+        prev_frame = num_frames - 1
+        A[:, 0] = subspace_z[m, 1:prev_frame]
+        A[:, 1] = np.diff(subspace_z[m, 0:prev_frame])
+        b = subspace_z[m, 2:num_frames]
 
         # x = [alpha_m, beta_m]
-        X[i, :] = least_squares(A, b)[0]
+        X[m, :] = least_squares(A, b)[0]
 
-    return X
+    alphas = X[:, 0]
+    betas = X[:, 1]
+
+    return alphas, betas
 
 
-def init_model(z, alpha, beta):
+def init_model_for_frame(
+    alphas: np.ndarray,
+    betas: np.ndarray,
+    z_star_prev: np.ndarray,
+    z_star_prev_prev: np.ndarray,
+) -> np.ndarray:
     """
     Calculate initial model z_bar = alpha ⊙ z_t−1 + beta ⊙ (zt−1 − zt−2)
     @param alpha: (n_components x 1) vector with the alpha factors
     @param beta: (n_components x 1) vector with the beta factors
-    @param z: (n_components x n) input data. Where n_components are pca components and n the number of data samples
-    @return z_bar: (n_components x n-2) initial model vector
+    @return z_bar: (n_components x 1) initial model vector for current frame
     """
-    n_components, n = z.shape
-    z_bar = np.zeros((n_components, n-2))
-    for i in range(2, n):
-        z_bar[:, i-2] = alpha * z[:, i-1] + beta * (z[:, i-1] - z[:, i-2])
+    z_bar = alphas * z_star_prev + betas * (z_star_prev - z_star_prev_prev)
     return z_bar
