6b88267cdde99849000ca3014aab6b15dfadf0ab
Joppe Geluykens <joppe@predibase.com>
2022-06-13 12:50:55 +0000

    wip train

--- neural_physics/tests/test_train.py
+++ neural_physics/tests/test_train.py
@@ -1,5 +1,11 @@
+import numpy as np
 import torch
+from neural_physics.core_math.pca import PCA
 from neural_physics.train.subspace_neural_physics import SubSpaceNeuralNetwork
+from neural_physics.utils.data_preprocess import (
+    init_model_for_frame,
+    initial_model_params,
+)
 
 
 def test_model():
@@ -7,7 +13,47 @@ def test_model():
     n_components = 256
 
     inputs = torch.rand(batch_size, n_components)
-    model = SubSpaceNeuralNetwork(n_hidden_layers=2, n_components=n_components)
+    model = SubSpaceNeuralNetwork(n_hidden_layers=2, num_components=n_components)
     outputs = model(inputs)
 
     assert outputs.shape == (batch_size, n_components)
+
+
+def test_train(dummy_data):
+    X = dummy_data
+
+    num_components = 8
+    pca = PCA(num_components)
+    pca.fit(X)
+    subspace_z = pca.encode(X)
+    # pca = PCA(n_components)
+    # pca.fit(Y)
+    # W = pca.encode(Y)
+
+    alphas, betas = initial_model_params(subspace_z)
+
+    network_correction = SubSpaceNeuralNetwork(
+        n_hidden_layers=2, num_components=num_components
+    )
+
+    window_size = 10
+
+    # Window size must be a divisor of the number of frames.
+    if subspace_z.shape[1] % window_size != 0:
+        # drop remainder of frames
+        subspace_z = subspace_z[:, : -subspace_z.shape[1] % window_size]
+
+    for subspace_z_window in np.array_split(
+        subspace_z, subspace_z.shape[1] // window_size
+    ):
+        z_star = np.zeros((num_components, window_size))
+        z_star[:, 0] = subspace_z_window[:, 0]
+        z_star[:, 1] = subspace_z_window[:, 1]
+
+        for frame in range(2, window_size):
+            z_star_prev = z_star[:, frame - 1]
+            z_star_prev_prev = z_star[:, frame - 2]
+            z_bar = init_model_for_frame(alphas, betas, z_star_prev, z_star_prev_prev)
+            model_inputs = np.concatenate((z_bar, z_star_prev)).T
+            model_inputs = torch.from_numpy(model_inputs).float().unsqueeze(0)
+            z_star[:, frame] = z_bar + network_correction(model_inputs)  # , w_i)
--- neural_physics/train/subspace_neural_physics.py
+++ neural_physics/train/subspace_neural_physics.py
@@ -10,21 +10,23 @@ print(f"Using {device} device")
 
 
 class SubSpaceNeuralNetwork(nn.Module):
-    def __init__(self, n_hidden_layers: int, n_components: int):
+    def __init__(self, n_hidden_layers: int = 10, num_components: int = 256):
         super().__init__()
 
-        hidden_units = round(1.5 * n_components)
+        self.num_components = num_components
 
-        self.encode = nn.Linear(n_components, hidden_units)
+        hidden_size = round(1.5 * self.num_components)
+
+        self.encode = nn.Linear(self.num_components, hidden_size)
 
         self.feed_forward = nn.Sequential(
             *[
-                nn.Sequential(nn.Linear(hidden_units, hidden_units), nn.ReLU())
+                nn.Sequential(nn.Linear(hidden_size, hidden_size), nn.ReLU())
                 for _ in range(n_hidden_layers)
             ],
         )
 
-        self.decode = nn.Linear(hidden_units, n_components)
+        self.decode = nn.Linear(hidden_size, self.num_components)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         """
@@ -33,6 +35,10 @@ class SubSpaceNeuralNetwork(nn.Module):
         @param inputs: The input to the network. Shape: (batch_size, n_components)
         @return: The output of the network. Shape: (batch_size, n_components)
         """
+        assert (
+            inputs.shape[1] == self.num_components
+        ), f"num_components in input: {inputs.shape[1]} does not match expected num_components: {self.num_components}"
+
         hidden = self.encode(inputs)  # (batch_size, hidden_units)
         hidden = self.feed_forward(hidden)  # (batch_size, hidden_units)
         logits = self.decode(hidden)  # (batch_size, n_components)
